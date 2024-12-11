ebefd8ae9cea36b539079c75a1c60ae192dd6a64
istamosh <63596969+istamosh@users.noreply.github.com>
2024-12-11 08:06:45 +0000

    update: trying to add /registration handlers

--- server/routes/registration.ts
+++ server/routes/registration.ts
@@ -1,8 +1,35 @@
 import express from "express";
 const router = express.Router();
 
+router.use(express.json());
+
+const payload = {
+  status: 102,
+  message: "",
+  data: null,
+};
+
 router.post("/", (req, res) => {
-  res.send(`got your POST request: ${JSON.stringify(req.body)}`);
+  if (!req.is("application/json"))
+    res.status(400).send({
+      ...payload,
+      message: "Invalid request body, you should use application/json",
+    });
+
+  const { email, first_name, last_name, password } = req.body;
+
+  const fields = { email, first_name, last_name, password };
+  for (const [key, value] of Object.entries(fields)) {
+    if (value === undefined)
+      res
+        .status(400)
+        .send({ ...payload, message: `${key} field is missing from request` });
+  }
+
+  // res.send(req.body);
+  res.send(
+    `Email: ${email}, First Name: ${first_name}, Last Name: ${last_name}, Password: ${password}`
+  );
 });
 
 export default router;
--- server/server.ts
+++ server/server.ts
@@ -3,8 +3,6 @@ import express from "express";
 const app = express();
 const port = 3001;
 
-app.use(express.json());
-
 app.get("/", (req, res) => {
   res.send("bonjour Istamosh!");
 });
